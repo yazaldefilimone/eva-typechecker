6d7672a05bb4e81305c3502ddffa044da02f7e73
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-19 23:02:30 +0200

    *

--- src/eva-typechecker.js
+++ src/eva-typechecker.js
@@ -1,15 +1,28 @@
 export class EvaTypechecker {
-  // infering and 
-  checker(expression){
+  // infering and
+  checker(expression) {
     /**
      * Number 10
      */
 
-    if(this._isNumber(expression)){
-      return 'number'
+    if (this._isNumber(expression)) {
+      return 'number';
     }
+
+    /**
+     * String "hello"
+     */
+
+    if (this._isString(expression)) {
+      return 'string';
+    }
+
+    throw new Error(`Unknown type for sexpression: ${expression}`);
   }
-  _isNumber(expression){
-    return typeof expression === 'number'
+  _isNumber(expression) {
+    return typeof expression === 'number';
+  }
+  _isString(expression) {
+    return typeof expression === 'string';
   }
 }
