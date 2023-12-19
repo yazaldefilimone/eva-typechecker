46e27e5b08b66be8564c46cc57a4bb148b076b84
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-19 23:50:02 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -1,11 +1,26 @@
-import assert from "node:assert";
-import {  EvaTypechecker } from "../src/eva-typechecker.js";
-import { Type } from "../src/type.js";
+import { readdirSync } from "node:fs";
+import path from "node:path";
 
+import {  EvaTypechecker } from "../src/eva-typechecker.js";
 
 
 const evaTypechecker = new EvaTypechecker();
 
-assert.deepEqual(evaTypechecker.checker(10), Type.number);
-assert.deepEqual(evaTypechecker.checker('"hello"'), Type.string);
+function loadFileWithEndsWith(path, endsWith){
+  const files = readdirSync(path);
+
+  return files.filter(file => file.endsWith(endsWith));
+}
+function autoLoadTests(){
+  const cwd = path.resolve(process.cwd(), 'tests')
+ const tests = loadFileWithEndsWith(cwd, ".test.js");
+ const execPromise =  tests.map(async test => {
+    const testModule = await import(path.resolve(cwd, test));
+    testModule.default(evaTypechecker);
+  });
+
+  Promise.all(execPromise).then()
+}
+
+autoLoadTests();
 console.log("All tests passed!")
