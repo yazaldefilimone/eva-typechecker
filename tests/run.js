83afdee1c69b20d137b1991652994bbe841a0eb6
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-19 23:02:30 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -6,4 +6,6 @@ import {  EvaTypechecker } from "../src/eva-typechecker.js";
 const evaTypechecker = new EvaTypechecker();
 
 assert.equal(evaTypechecker.checker(10), "number");
+assert.equal(evaTypechecker.checker("hello"), "string");
+assert.equal(evaTypechecker.checker(true), "boolean");
 console.log("All tests passed!")
