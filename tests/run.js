ced50e0a522ea4f81c7ea65a57b5d3d2ab93a8b4
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-22 23:38:53 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -11,16 +11,16 @@ function loadFileWithEndsWith(path, endsWith){
 
   return files.filter(file => file.endsWith(endsWith));
 }
-function autoLoadTests(){
+async function autoLoadTests(){
   const cwd = path.resolve(process.cwd(), 'tests')
  const tests = loadFileWithEndsWith(cwd, ".test.js");
- const execPromise =  tests.map(async test => {
-    const testModule = await import(path.resolve(cwd, test));
-    testModule.default(evaTypechecker);
-  });
+ const execPromise =tests.map(async test =>  await import(path.resolve(cwd, test)))
 
-  Promise.all(execPromise).then()
+ for await  (const testModule of execPromise) {
+    testModule.default(evaTypechecker);
+}
 }
 
-autoLoadTests();
+autoLoadTests().then(() => {
 console.log("All tests passed!")
+});
