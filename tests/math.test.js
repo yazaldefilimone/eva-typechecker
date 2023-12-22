b75893a4fa5f998bbad788933f9b61d8aff412b9
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-22 23:38:53 +0200

    *

---
+++ tests/math.test.js
@@ -0,0 +1,14 @@
+import { test } from "./utils.js"
+import { Type } from "../src/type.js";
+
+export default (evaTypechecker) => {
+  test(evaTypechecker, ["+", 1, 1], Type.number);
+  test(evaTypechecker, ["-", 1, 2], Type.number);
+  test(evaTypechecker, ["*", 1, 2], Type.number);
+  test(evaTypechecker, ["/", 1, 2], Type.number);
+
+  // strings 
+  test(evaTypechecker, ["+", `"hello"`, `"world"`], Type.string);
+  test(evaTypechecker, ["-", `"hello"`, `"world"`], Type.string);
+
+}
