e12cfb496fed450863f8d320247381b34aa2a307
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-27 22:38:52 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -3,24 +3,24 @@ import path from "node:path";
 
 import {  EvaTypechecker } from "../src/eva-typechecker.js";
 
-
 const evaTypechecker = new EvaTypechecker();
-
 function loadFileWithEndsWith(path, endsWith){
   const files = readdirSync(path);
-
-  return files.filter(file => file.endsWith(endsWith));
+  const tests = files.filter(file => file.endsWith(endsWith));
+  return tests;
 }
-async function autoLoadTests(){
-  const cwd = path.resolve(process.cwd(), 'tests')
- const tests = loadFileWithEndsWith(cwd, ".test.js");
- const execPromise =tests.map(async test =>  await import(path.resolve(cwd, test)))
-
- for await  (const testModule of execPromise) {
+async function autoLoadTests() {
+  const cwd = path.resolve(process.cwd(), 'tests');
+  const tests = loadFileWithEndsWith(cwd, ".test.js");
+  
+  for (const test of tests) {
+    const testModule = await import(path.resolve(cwd, test));
     testModule.default(evaTypechecker);
-}
+  }
+  console.log("All tests passed!");
 }
 
-autoLoadTests().then(() => {
-console.log("All tests passed!")
-});
+autoLoadTests().catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
