bca787bd7697d4f301c3fb6cdcf616082de4f77e
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-27 22:38:52 +0200

    *

--- tests/variable.test.js
+++ tests/variable.test.js
@@ -3,7 +3,10 @@ import { Type } from "../src/type.js";
 
 export default (evaTypechecker) => {
   test(evaTypechecker, ["var", "x", 10], Type.number);
-  test(evaTypechecker, ["x"], Type.number);
+  test(evaTypechecker, "x", Type.number);
+  test(evaTypechecker, ["var", ["y", "string"], `"hello"`], Type.string);
+  test(evaTypechecker, "y", Type.string);
+  test(evaTypechecker, ["var", ["y", "string"], 10], Type.string);
   // global variable
-  test(evaTypechecker, [`"VERSION"`], Type.string);
+  test(evaTypechecker, `VERSION`, Type.string);
 }
