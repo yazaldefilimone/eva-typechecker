0bc1ce16606c15c18dc01b66b6ef47c6b633439d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-07 22:32:50 +0200

    *

--- tests/self-eval.test.js
+++ tests/self-eval.test.js
@@ -4,4 +4,6 @@ import { Type } from "../src/type.js";
 export default (evaTypechecker) => {
   test(evaTypechecker, 10, Type.number);
   test(evaTypechecker, '"hello"', Type.string);
+  test(evaTypechecker, true, Type.boolean);
+  test(evaTypechecker, false, Type.boolean);
 }
