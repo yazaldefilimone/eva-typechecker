e251c7bba8fc478fe61fbcc1ebad1f3c3b979038
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-07 22:32:50 +0200

    *

--- src/eva-typechecker.js
+++ src/eva-typechecker.js
@@ -5,6 +5,16 @@ export class EvaTypechecker {
   constructor() {
     this.global = this._createGlobalEnv();
   }
+
+  checkerGlobal(expression) {
+    return this._checherGlobal(expression, this.global);
+  }
+  _checherGlobal(expression, env) {
+    if (this._isKeyword(expression, 'begin')) {
+      return this._checkerBlock(expression, env);
+    }
+    return this.checker(expression, env);
+  }
   // infering and
   checker(expression, env = this.global) {
     /**
@@ -23,25 +33,42 @@ export class EvaTypechecker {
       return Type.string;
     }
 
+    /**
+     * Boolean true
+     * Boolean false
+     **/
+    if (this._isBoolean(expression)) {
+      return Type.boolean;
+    }
+
+    if (this._isBoolanBinary(expression)) {
+      return this._booleanBinary(expression, env);
+    }
+
     if (this._isBinary(expression)) {
       return this._Binary(expression, env);
     }
+
     if (this._isKeyword(expression, 'var')) {
       return this._variableDeclaration(expression, env);
     }
+
     if (this._isKeyword(expression, 'begin')) {
       const blockEnv = env.extend();
       return this._checkerBlock(expression, blockEnv);
     }
+
     if (this._isKeyword(expression, 'set')) {
       const [_tag, refName, value] = expression;
       const refType = this.checker(refName, env);
       const valueType = this.checker(value, env);
       return this._expect(refType, valueType, value, expression);
     }
+
     if (this._isVariable(expression)) {
       return env.lookup(expression);
     }
+
     throw `Unknown type for sexpression: ${expression}`;
   }
 
@@ -75,6 +102,14 @@ export class EvaTypechecker {
       throw `Unexpected type ${_type} in ${expression}, allowed: ${allowedTypes}`;
     }
   }
+  _booleanBinary(expression, env) {
+    this._checkArity(expression, 2);
+    const [operator, left, right] = expression;
+    const leftType = this.checker(left, env);
+    const rightType = this.checker(right, env);
+    this._expect(leftType, rightType, left, expression);
+    return Type.boolean;
+  }
   _checkArity(expression, arity) {
     const argsSize = expression.length - 1;
     if (argsSize !== arity) {
@@ -93,6 +128,19 @@ export class EvaTypechecker {
     return new TypeEnvironment({
       VERSION: Type.string,
     });
+  }
+  _isBoolanBinary(expression) {
+    switch (expression[0]) {
+      case '>':
+      case '<':
+      case '>=':
+      case '<=':
+      case '==':
+      case '!=':
+        return true;
+      default:
+        return false;
+    }
   }
   _variableDeclaration(expression, env) {
     const [_tag, nameExpression, value] = expression;
@@ -124,6 +172,9 @@ export class EvaTypechecker {
   }
   _isNumber(expression) {
     return typeof expression === 'number';
+  }
+  _isBoolean(expression) {
+    return typeof expression === 'boolean' || expression === 'true' || expression === 'false';
   }
   _isString(expression) {
     if (typeof expression !== 'string') {
