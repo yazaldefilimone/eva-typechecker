834bf5917ee5bfcff0e72850e8d0a4cca5cd737d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-13 22:19:33 +0200

    *

--- tests/if-expression.test.js
+++ tests/if-expression.test.js
@@ -4,4 +4,10 @@ import { Type } from "../src/type.js";
 export default (evaTypechecker) => {
   const code = `(<= 1 2)`;
   test(evaTypechecker, code, Type.boolean);
+  const ifExpression = `
+    (if (<= 1 2)
+      (+ 1 2)
+      (- 1 2))
+  `
+  test(evaTypechecker, ifExpression, Type.number);
 }
