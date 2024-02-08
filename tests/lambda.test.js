d7ce37ac6aa0e74b417a53223a36b29c03ba0f41
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-08 10:49:07 +0200

    *

--- tests/lambda.test.js
+++ tests/lambda.test.js
@@ -2,10 +2,8 @@ import { test } from "./utils.js"
 import { Type } from "../src/type.js";
 
 export default (evaTypechecker) => {
-
   let squareLambda = `
-  (lambda ((x number)) -> number (+ x y))
+  (lambda ((x number)) -> number (+ x 10))
   `
-
   test(evaTypechecker, squareLambda, Type.formString('Fn<number<number>>'));
 }
