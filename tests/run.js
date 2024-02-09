3ee56b3e61d8a90f282302529ee3660a60bfa391
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-10 01:51:21 +0200

    refactor: variable name in tests/run.js

--- tests/run.js
+++ tests/run.js
@@ -3,7 +3,7 @@ import path from "node:path";
 
 import {  EvaTypechecker } from "../src/eva-typechecker.js";
 
-const evaTypechecker = new EvaTypechecker();
+const evaTypeChecker = new EvaTypechecker();
 function loadFileWithEndsWith(path, endsWith){
   const files = readdirSync(path);
   const tests = files.filter(file => file.endsWith(endsWith));
@@ -12,15 +12,19 @@ function loadFileWithEndsWith(path, endsWith){
 async function autoLoadTests() {
   const cwd = path.resolve(process.cwd(), 'tests');
   const tests = loadFileWithEndsWith(cwd, ".test.js");
-  
   for (const test of tests) {
+    // console.log(`running ${test}`);
     const testModule = await import(path.resolve(cwd, test));
-    testModule.default(evaTypechecker);
+    testModule.default(evaTypeChecker);
   }
   console.log("All tests passed!");
 }
 
 autoLoadTests().catch((error) => {
-  console.error(error);
-  process.exit(1);
+  if (process.argv0.includes('node')) {
+    const errorRed = `\x1b[31m${`error`}\x1b[0m: ${error}`
+    console.log(errorRed);
+    process.exit(1);
+  }
+  throw error;
 })
