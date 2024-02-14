96898e8670ee7fff3c0800e6205d933e3c0cd386
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-15 00:51:00 +0200

    Refactor generics.test.js and add testFn

--- tests/generics.test.js
+++ tests/generics.test.js
@@ -11,12 +11,25 @@ export default (evaTypechecker) => {
  test(evaTypechecker, "(generic <number> 5 5)", Type.number);
   test(evaTypechecker, `(generic <string> "Hello, " "World")`, Type.string);
 
-  const lambdaNumberGenerics = `
-  ((lambda <T> ((n T)) -> T  (+ n 1)) <number> 10)
+  // const lambdaNumberGenerics = `
+  // ((lambda <T> ((n T)) -> T  (+ n 1)) <number> 10)
+  // `
+  // const lambdaStringGenerics = `
+  // ((lambda <T> ((n T)) -> T  (+ n " World")) <string> "Hello")
+  // `
+  // test(evaTypechecker, lambdaNumberGenerics ,Type.number);
+  // test(evaTypechecker, lambdaStringGenerics, Type.string);
+
+
+
+  const testFn =  `
+  (def test <T> ((type T)) -> number
+    (* type 2)
+  )
+  (test 10)
   `
-  const lambdaStringGenerics = `
-  ((lambda <T> ((n T)) -> T  (+ n " World")) <string> "Hello")
-  `
-  test(evaTypechecker, lambdaNumberGenerics ,Type.number);
-  test(evaTypechecker, lambdaStringGenerics, Type.string);
+
+
+
+  test(evaTypechecker, testFn, Type.number)
 }
